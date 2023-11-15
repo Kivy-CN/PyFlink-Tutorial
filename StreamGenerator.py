3a3793ca380d5e2f76a43d38e921ba14e550058c
Frederick <softcheck@outlook.com>
2023-11-15 21:20:19 +0800

    Update StreamGenerator.py

--- StreamGenerator.py
+++ StreamGenerator.py
@@ -21,13 +21,12 @@ def send_file_to_kafka(file_path: str, topic: str, bootstrap_servers: str):
                     break
                 # Send the data to the given topic
                 producer.send(topic, data)
-                # Print the number of bytes sent to the topic
-                bytes_sent = len(data)
-                print(f"Sent {bytes_sent} bytes to Kafka topic {topic}")
                 # Calculate the percentage of the file sent
                 percent_sent = (f.tell() / file_size) * 100
-                # Print the percentage of the file sent
-                print(f"{percent_sent:.2f}% of the file sent")
+                
+                # Print the number of bytes sent to the topic and thepercentage of the file sent
+                bytes_sent = len(data)
+                print(f"Sent {bytes_sent} bytes  {topic} {percent_sent:.2f}% sent")
                 # Wait for 3 seconds
                 time.sleep(3)
         # Wait for user input to continue or exit
