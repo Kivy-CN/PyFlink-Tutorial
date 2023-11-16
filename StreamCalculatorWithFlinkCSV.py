1ba731cbfe0459ec70e2cc104f111b77e2194b70
Frederick <softcheck@outlook.com>
2023-11-17 07:42:01 +0800

    update

--- StreamCalculatorWithFlinkCSV.py
+++ StreamCalculatorWithFlinkCSV.py
@@ -1,13 +1,3 @@
-import os
-# Get current absolute path
-current_file_path = os.path.abspath(__file__)
-# Get current dir path
-current_dir_path = os.path.dirname(current_file_path)
-# Change into current dir path
-os.chdir(current_dir_path)
-output_path = current_dir_path
-
-
 import re
 import argparse
 import logging
@@ -29,41 +19,31 @@ def split(line):
 def read_from_kafka():
     Year_Begin =1999
     Year_End = 2023
-    # Create a Flink execution environment
     env = StreamExecutionEnvironment.get_execution_environment()    
-
-    # Add the Flink SQL Kafka connector jar file to the classpath
     env.add_jars("file:///home/hadoop/Desktop/PyFlink-Tutorial/flink-sql-connector-kafka-3.1-SNAPSHOT.jar")
-
-    # Print a message to indicate that data reading from Kafka has started
     print("start reading data from kafka")
-
-    # Create a Kafka consumer
     kafka_consumer = FlinkKafkaConsumer(
-        topics='data', # The topic to consume messages from
-        deserialization_schema= SimpleStringSchema('UTF-8'), # The schema to deserialize messages
-        properties={'bootstrap.servers': 'localhost:9092', 'group.id': 'my-group'} # The Kafka broker address and consumer group ID
+        topics='data', 
+        deserialization_schema= SimpleStringSchema('UTF-8'),
+        properties={'bootstrap.servers': 'localhost:9092', 'group.id': 'my-group'} 
     )
-
-    # Start reading messages from the earliest offset
     kafka_consumer.set_start_from_earliest()
-
-    # 重定向sys.stdout到一个io.StringIO对象
     output = StringIO()
     sys.stdout = output
-
-    # Add the Kafka consumer as a source to the Flink execution environment and print the messages to the console
     env.add_source(kafka_consumer).map(lambda x: ' '.join(re.findall(r'\d+', x))).filter(lambda x: any([Year_Begin <= int(i) <= Year_End for i in x.split()])).map(lambda x:  [i for i in x.split() if Year_Begin <= int(i) <= Year_End][0]).print()
-
-    # 恢复sys.stdout
     sys.stdout = sys.__stdout__
-
-    # 输出捕获的内容
     print(output.getvalue())
-    
-    # submit for execution
+    df = pd.read_csv(StringIO(output.getvalue()), sep=" ", header=None)
+    df.columns = ["year"]
+    unique_years = df["year"].unique()
+    unique_years_count = df["year"].nunique()
+    unique_years_sorted = df["year"].value_counts().sort_values(ascending=False).index.tolist()
+
+    print(f"当前所获得数据中的不重复年份有 {unique_years_count} 个，从多到少排列如下：")
+    for year in unique_years_sorted:
+        print(f"{year} 出现了 {df['year'].value_counts()[year]} 次")
+
     env.execute()
 
 if __name__ == '__main__':
-    # logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
     read_from_kafka()
--- tmp.py
+++ tmp.py
@@ -1,3 +1,11 @@
+import os
+# Get current absolute path
+current_file_path = os.path.abspath(__file__)
+# Get current dir path
+current_dir_path = os.path.dirname(current_file_path)
+# Change into current dir path
+os.chdir(current_dir_path)
+output_path = current_dir_path
 import sys
 from io import StringIO
 
