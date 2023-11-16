28f9764a95dc0b0f8474f5ec466e671ac1d7dd29
Frederick <softcheck@outlook.com>
2023-11-17 07:53:33 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,23 +1,54 @@
-import os
-# Get current absolute path
-current_file_path = os.path.abspath(__file__)
-# Get current dir path
-current_dir_path = os.path.dirname(current_file_path)
-# Change into current dir path
-os.chdir(current_dir_path)
-output_path = current_dir_path
+import re
+import argparse
+import logging
 import sys
 from io import StringIO
+import numpy as np 
+import pandas as pd
+from pyflink.table import StreamTableEnvironment
+from pyflink.common import WatermarkStrategy, Encoder, Types
+from pyflink.datastream import StreamExecutionEnvironment, RuntimeExecutionMode
+from pyflink.datastream.connectors.file_system import FileSource, StreamFormat, FileSink, OutputFileConfig, RollingPolicy
+from pyflink.common import Types, SimpleStringSchema
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.connectors.kafka import FlinkKafkaProducer, FlinkKafkaConsumer
 
-# 重定向sys.stdout到一个io.StringIO对象
-output = StringIO()
-sys.stdout = output
+def split(line):
+    yield from line.split()
 
-# 在这里运行您的代码，所有的stdout输出都会被捕获
-print("Hello, world!")
+def read_from_kafka():
+    Year_Begin =1999
+    Year_End = 2023
+    env = StreamExecutionEnvironment.get_execution_environment()    
+    env.add_jars("file:///home/hadoop/Desktop/PyFlink-Tutorial/flink-sql-connector-kafka-3.1-SNAPSHOT.jar")
+    print("start reading data from kafka")
+    kafka_consumer = FlinkKafkaConsumer(
+        topics='data', 
+        deserialization_schema= SimpleStringSchema('UTF-8'),
+        properties={'bootstrap.servers': 'localhost:9092', 'group.id': 'my-group'} 
+    )
+    kafka_consumer.set_start_from_earliest()
+    
+    data_stream =  env.add_source(kafka_consumer).map(lambda x: ' '.join(re.findall(r'\d+', x))).filter(lambda x: any([Year_Begin <= int(i) <= Year_End for i in x.split()])).map(lambda x:  [i for i in x.split() if Year_Begin <= int(i) <= Year_End][0])
+    
 
-# 恢复sys.stdout
-sys.stdout = sys.__stdout__
+    # 创建一个新的图形
+    fig, ax = plt.subplots()
 
-# 输出捕获的内容
-print(output.getvalue())
+    # 绘制动态折线图
+    def update(data):
+        ax.clear()
+        ax.plot(data)
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Value')
+        ax.set_title('Data Visualization')
+        plt.pause(0.1)
+
+    data_stream.map(lambda x: float(x)).add_sink(lambda x: update(x))
+
+    # 显示图形
+    plt.show()
+    env.execute()
+
+if __name__ == '__main__':
+    read_from_kafka()
