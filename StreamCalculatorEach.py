d66f682cddd9fa27db45fbbbc7e96d6de1965c01
Frederick <softcheck@outlook.com>
2023-11-16 08:32:22 +0800

    update

---
+++ StreamCalculatorEach.py
@@ -0,0 +1,51 @@
+import os
+import re
+from collections import Counter
+from pyflink.table import StreamTableEnvironment
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.connectors.kafka import FlinkKafkaConsumer
+from pyflink.common import SimpleStringSchema
+
+def remove_punctuation(text):
+    return re.sub(r'[^\w\s]','',text)
+
+def count_words(text):
+    words = text.split()
+    return Counter(words)
+
+def read_from_kafka():
+    # Create a Flink execution environment
+    env = StreamExecutionEnvironment.get_execution_environment()    
+
+    # Add the Flink SQL Kafka connector jar file to the classpath
+    env.add_jars("file:///home/hadoop/Desktop/PyFlink-Tutorial/flink-sql-connector-kafka-3.1-SNAPSHOT.jar")
+
+    # Print a message to indicate that data reading from Kafka has started
+    print("start reading data from kafka")
+
+    # Create a Kafka consumer
+    kafka_consumer = FlinkKafkaConsumer(
+        topics='hamlet', # The topic to consume messages from
+        deserialization_schema= SimpleStringSchema('UTF-8'), # The schema to deserialize messages
+        properties={'bootstrap.servers': 'localhost:9092', 'group.id': 'my-group'} # The Kafka broker address and consumer group ID
+    )
+
+    # Start reading messages from the earliest offset
+    kafka_consumer.set_start_from_earliest()
+
+    # Add the Kafka consumer as a source to the Flink execution environment
+    stream = env.add_source(kafka_consumer)
+
+    # Remove punctuation from the text
+    stream_remove_punctuation = stream.map(lambda x: remove_punctuation(x))
+
+    # Count the words in the text
+    stream_count_words = stream_remove_punctuation.map(lambda x: count_words(x))
+
+    # Print the word counts to the console
+    stream_count_words.print()
+
+    # Start the Flink job
+    env.execute()
+
+read_from_kafka()
