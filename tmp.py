87108b8d713b41cff061fbf0a1c5e8035599de31
Frederick <softcheck@outlook.com>
2023-11-20 12:37:58 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -11,14 +11,15 @@ class MyKeyedProcessFunction(KeyedProcessFunction):
         out.collect(value)
 
 env = StreamExecutionEnvironment.get_execution_environment()
-# Add the Flink SQL Kafka connector jar file to the classpath
 env.add_jars("file:///home/hadoop/Desktop/PyFlink-Tutorial/flink-sql-connector-kafka-3.1-SNAPSHOT.jar")
 
 kafka_consumer = FlinkKafkaConsumer(
     topics='data',
-    deserialization_schema=SimpleStringSchema(),
+    deserialization_schema = SimpleStringSchema('UTF-8'),
     properties={'bootstrap.servers': 'localhost:9092', 'group.id': 'my-group'}
 )
+
+kafka_consumer.set_start_from_earliest()
 data_stream = env.add_source(kafka_consumer)
 keyed_stream = data_stream.key_by(lambda x: x[0])
 keyed_stream.process(MyKeyedProcessFunction()).print()
