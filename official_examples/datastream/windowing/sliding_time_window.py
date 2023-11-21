7bb7c8f783d4135ef2dec6a60615fe32292e7af0
Frederick <softcheck@outlook.com>
2023-11-21 11:04:12 +0800

    Update sliding_time_window.py

--- official_examples/datastream/windowing/sliding_time_window.py
+++ official_examples/datastream/windowing/sliding_time_window.py
@@ -36,6 +36,22 @@ class MyTimestampAssigner(TimestampAssigner):
 
 
 # 定义一个CountWindowProcessFunction类，继承自ProcessWindowFunction[tuple, tuple, str, TimeWindow]类，用于计算窗口内的元素数量
+# `CountWindowProcessFunction`类是一个窗口处理函数，用于计算窗口内的元素数量。该类接收四个参数：
+# - `key`：表示窗口的键。
+# - `context`：表示窗口的上下文，包括窗口的开始时间、结束时间等信息。
+# - `elements`：表示窗口内的元素，是一个可迭代对象。
+# - `out`：表示输出结果的`Collector`对象。
+# `CountWindowProcessFunction`类的`process`方法用于计算窗口内的元素数量。
+# 该方法接收三个参数：`key`、`context`和`elements`。
+# 可以使用`elements`参数计算窗口内的元素数量，并将结果输出到`out`参数中。
+# 输出的结果是一个元组，包含窗口的键、窗口的开始时间、窗口的结束时间以及窗口内的元素数量。
+# 运算过程如下：
+# 1. 将窗口内的元素保存在一个可迭代对象中。
+# 2. 计算可迭代对象的长度，即窗口内的元素数量。
+# 3. 将窗口的键、开始时间、结束时间和元素数量组成一个元组。
+# 4. 将元组输出到`out`参数中。
+# `CountWindowProcessFunction`类的`process`方法是在窗口关闭时调用的，因此在该方法中可以访问窗口内的所有元素。
+
 class CountWindowProcessFunction(ProcessWindowFunction[tuple, tuple, str, TimeWindow]):
     # 重写process方法，用于计算窗口内的元素数量
     def process(self,
@@ -81,7 +97,7 @@ if __name__ == '__main__':
     # 定义窗口，并设置窗口处理函数
     ds = data_stream.assign_timestamps_and_watermarks(watermark_strategy) \
         .key_by(lambda x: x[0], key_type=Types.STRING()) \
-        .window(SlidingEventTimeWindows.of(Time.milliseconds(5), Time.milliseconds(2))) \
+        .window(SlidingEventTimeWindows.of(Time.seconds(5), Time.seconds(2))) \
         .process(CountWindowProcessFunction(),
                  Types.TUPLE([Types.STRING(), Types.INT(), Types.INT(), Types.INT()]))
 
