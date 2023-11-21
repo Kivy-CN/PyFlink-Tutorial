2857add863209f2f222a1b13091b83336b27d719
Frederick <softcheck@outlook.com>
2023-11-21 10:40:19 +0800

    Update Transaction_Steam_Calculator.py

--- Transaction_Steam_Calculator.py
+++ Transaction_Steam_Calculator.py
@@ -73,7 +73,7 @@ def check_data(data):
     # # col_target = transpose_data[3]
     # col_target = [row[3] for row in data] 
     # print(f"column target is: {col_target[0]} ",f" typeis: {type(col_target[0])}")
-    print(f"data[0] type is {type(data[0])}",f"data[0][1] type is {type(data[0][1])}",f"data[0] len is {len(data[0])}")
+    print(f"data[0] type is {type(data[0])}",f"data[0][3] type is {type(data[0][3])}",f"data[0] len is {len(data[0])}")
     return data
 
 def parse_tuple(x):
@@ -113,8 +113,7 @@ def read_from_kafka():
     # type_info = checked_stream.get_type()
     # print(type_info)
 
-    ds = checked_stream.map(parse_tuple, \
-        output_type=Types.TUPLE([Types.FLOAT(),Types.STRING(), Types.FLOAT(), Types.FLOAT(), Types.STRING(), Types.STRING()]))
+    ds = checked_stream.map(parse_tuple)
     ds.print()
     env.execute()
 
