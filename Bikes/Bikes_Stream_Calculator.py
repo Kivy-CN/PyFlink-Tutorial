c0eed7d524e301f910ce3ee4acb132e8e20606f5
Frederick <softcheck@outlook.com>
2024-05-20 13:48:34 +0800

    Update Bikes_Stream_Calculator.py

--- Bikes/Bikes_Stream_Calculator.py
+++ Bikes/Bikes_Stream_Calculator.py
@@ -58,7 +58,8 @@ def haversine(lon1, lat1, lon2, lat2):
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
-    return c * r
+    distance = round(c * r, 3)  # round the result to 3 decimal places
+    return f"{distance} km"  # return the distance with unit
 
 def calculate_distance(x):
     """
